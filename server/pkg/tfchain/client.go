bc544b7477d0e6a535835afd88459a94fdfdb070
brandonpille <brandon@threefold.tech>
2023-04-11 18:03:25 +0200

    server side tfchain

--- server/pkg/tfchain/client.go
+++ server/pkg/tfchain/client.go
@@ -28,6 +28,66 @@ type (
 		identity *substrate.Identity
 		network  string
 	}
+
+	Transfer struct {
+		amount      uint64
+		destination string
+	}
+
+	CreateTwin struct {
+		relay string
+		pk    []byte
+	}
+
+	AcceptTermsAndConditions struct {
+		link string
+		hash string
+	}
+
+	GetContractWithHash struct {
+		nodeID uint32
+		hash   substrate.HexHash
+	}
+
+	CreateNodeContract struct {
+		nodeID             uint32
+		body               string
+		hash               string
+		publicIPs          uint32
+		solutionProviderID *uint64
+	}
+
+	CreateRentContract struct {
+		nodeID             uint32
+		solutionProviderID *uint64
+	}
+
+	ServiceContractCreate struct {
+		service  substrate.AccountID
+		consumer substrate.AccountID
+	}
+
+	ServiceContractBill struct {
+		contractID     uint64
+		variableAmount uint64
+		metadata       string
+	}
+
+	SetServiceContractFees struct {
+		contractID  uint64
+		baseFee     uint64
+		variableFee uint64
+	}
+
+	ServiceContractSetMetadata struct {
+		contractID uint64
+		metadata   string
+	}
+
+	CreateFarm struct {
+		name      string
+		publicIPs []substrate.PublicIPInput
+	}
 )
 
 // Error implements the error interface
@@ -40,6 +100,20 @@ func NewClient() *Client {
 	return &Client{
 		state: state.NewStateManager[tfchainState](),
 	}
+}
+
+func tfchainNetworkFromNetworkString(ntwrk string) (string, error) {
+	if ntwrk == "mainnet" {
+		return tfchainMainnet, nil
+	} else if ntwrk == "testnet" {
+		return tfchainTestnet, nil
+	} else if ntwrk == "qanet" {
+		return tfchainQanet, nil
+	} else if ntwrk == "devnet" {
+		return tfchainDevnet, nil
+	}
+
+	return "", errors.New("unsupported network")
 }
 
 // Load a client, connecting to the rpc endpoint at the given URL and loading a keypair from the given mnemonic
@@ -72,18 +146,18 @@ func (c *Client) Load(ctx context.Context, network string, passphrase string) er
 }
 
 // Transer an amount of TFT from the loaded account to the destination.
-func (c *Client) Transfer(ctx context.Context, amount uint64, destination string) error {
+func (c *Client) Transfer(ctx context.Context, args Transfer) error {
 	state, ok := c.state.Get(state.IDFromContext(ctx))
 	if !ok || state.client == nil {
 		return pkg.ErrClientNotConnected{}
 	}
 
-	dest, err := substrate.FromAddress(destination)
+	dest, err := substrate.FromAddress(args.destination)
 	if err != nil {
 		return err
 	}
 
-	return state.client.Transfer(*state.identity, amount, dest)
+	return state.client.Transfer(*state.identity, args.amount, dest)
 }
 
 // Balance of an account for TFT on stellar.
@@ -115,6 +189,33 @@ func (c *Client) GetTwin(ctx context.Context, id uint32) (*substrate.Twin, error
 	return state.client.GetTwin(id)
 }
 
+func (c *Client) GetTwinByPubKey(ctx context.Context, pk []byte) (uint32, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.GetTwinByPubKey(pk)
+}
+
+func (c *Client) CreateTwin(ctx context.Context, args CreateTwin) (uint32, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.CreateTwin(*state.identity, args.relay, args.pk)
+}
+
+func (c *Client) AcceptTermsAndConditions(ctx context.Context, args AcceptTermsAndConditions) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.AcceptTermsAndConditions(*state.identity, args.link, args.hash)
+}
+
 func (c *Client) GetNode(ctx context.Context, id uint32) (*substrate.Node, error) {
 	state, ok := c.state.Get(state.IDFromContext(ctx))
 	if !ok || state.client == nil {
@@ -122,6 +223,23 @@ func (c *Client) GetNode(ctx context.Context, id uint32) (*substrate.Node, error
 	}
 
 	return state.client.GetNode(id)
+}
+
+func (c *Client) GetNodes(ctx context.Context, farm_id uint32) ([]uint32, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return []uint32{}, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.GetNodes(farm_id)
+}
+
+func (c *Client) CreateNode(ctx context.Context, node *substrate.Node) (uint32, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+	return state.client.CreateNode(*state.identity, *node)
 }
 
 func (c *Client) GetFarm(ctx context.Context, id uint32) (*substrate.Farm, error) {
@@ -133,16 +251,155 @@ func (c *Client) GetFarm(ctx context.Context, id uint32) (*substrate.Farm, error
 	return state.client.GetFarm(id)
 }
 
-func tfchainNetworkFromNetworkString(ntwrk string) (string, error) {
-	if ntwrk == "mainnet" {
-		return tfchainMainnet, nil
-	} else if ntwrk == "testnet" {
-		return tfchainTestnet, nil
-	} else if ntwrk == "qanet" {
-		return tfchainQanet, nil
-	} else if ntwrk == "devnet" {
-		return tfchainDevnet, nil
+func (c *Client) GetFarmByName(ctx context.Context, name string) (uint32, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
 	}
 
-	return "", errors.New("unsupported network")
+	return state.client.GetFarmByName(name)
+}
+
+func (c *Client) CreatetFarm(ctx context.Context, args CreateFarm) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.CreateFarm(*state.identity, args.name, args.publicIPs)
+}
+
+func (c *Client) GetContract(ctx context.Context, contract_id uint64) (*substrate.Contract, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return nil, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.GetContract(contract_id)
+}
+
+func (c *Client) GetContractIDByNameRegistration(ctx context.Context, name string) (uint64, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.GetContractIDByNameRegistration(name)
+}
+
+func (c *Client) GetContractWithHash(ctx context.Context, args GetContractWithHash) (uint64, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.GetContractWithHash(args.nodeID, args.hash)
+}
+
+func (c *Client) CreateNameContract(ctx context.Context, name string) (uint64, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.CreateNameContract(*state.identity, name)
+}
+
+func (c *Client) CreateNodeContract(ctx context.Context, args CreateNodeContract) (uint64, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.CreateNodeContract(*state.identity, args.nodeID, args.body, args.hash, args.publicIPs, args.solutionProviderID)
+}
+
+func (c *Client) CreateRentContract(ctx context.Context, args CreateRentContract) (uint64, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.CreateRentContract(*state.identity, args.nodeID, args.solutionProviderID)
+}
+
+func (c *Client) ServiceContractCreate(ctx context.Context, args ServiceContractCreate) (uint64, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return 0, pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.ServiceContractCreate(*state.identity, args.service, args.consumer)
+}
+
+func (c *Client) ServiceContractApprove(ctx context.Context, contract_id uint64) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.ServiceContractApprove(*state.identity, contract_id)
+}
+
+func (c *Client) ServiceContractBill(ctx context.Context, args ServiceContractBill) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.ServiceContractBill(*state.identity, args.contractID, args.variableAmount, args.metadata)
+}
+
+func (c *Client) ServiceContractCancel(ctx context.Context, contract_id uint64) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.ServiceContractCancel(*state.identity, contract_id)
+}
+
+func (c *Client) ServiceContractReject(ctx context.Context, contract_id uint64) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.ServiceContractReject(*state.identity, contract_id)
+}
+
+func (c *Client) ServiceContractSetFees(ctx context.Context, args SetServiceContractFees) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.ServiceContractSetFees(*state.identity, args.contractID, args.baseFee, args.variableFee)
+}
+
+func (c *Client) ServiceContractSetMetadata(ctx context.Context, args ServiceContractSetMetadata) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.ServiceContractSetMetadata(*state.identity, args.contractID, args.metadata)
+}
+
+func (c *Client) CancelContract(ctx context.Context, contract_id uint64) error {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.CancelContract(*state.identity, contract_id)
+}
+
+func (c *Client) GetZosVersion(ctx context.Context) (string, error) {
+	state, ok := c.state.Get(state.IDFromContext(ctx))
+	if !ok || state.client == nil {
+		return "", pkg.ErrClientNotConnected{}
+	}
+
+	return state.client.GetZosVersion()
 }
