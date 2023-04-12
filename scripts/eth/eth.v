5334c4b7216b1d400afe85defae88525db1b48ab
dylanVerstraete <dylan_verstraete1@hotmail.com>
2023-04-12 16:23:43 +0200

    feat: add erc721 (nft) support

--- scripts/eth/eth.v
+++ scripts/eth/eth.v
@@ -48,7 +48,6 @@ pub fn approve_token_spending(mut client RpcWsClient, contractAddress string, de
 
 // Multisig
 
-
 pub fn get_multisig_owners(mut client RpcWsClient, contractAddress string) ![]string {
 	_ := client.send_json_rpc[[]string, string]('eth.GetMultisigOwners', [contractAddress],
 		eth.default_timeout)!
@@ -86,5 +85,55 @@ pub fn initiate_multisig_token_transfer(mut client RpcWsClient, contractAddress 
 
 pub fn initiate_multisig_token_transfer(mut client RpcWsClient, contractAddress string, tokenAddress string, destination string, amount i64) !string {
 	_ := client.send_json_rpc[[]string, string]('eth.InitiateMultisigTokenTransfer', [contractAddress, tokenAddress, destination, amount],
+		eth.default_timeout)!
+}
+
+// Fungibles
+
+// GetFungibleBalance (ERC721)
+pub fn get_fungible_balance(mut client RpcWsClient, contractAddress string, address string) !i64 {
+	_ := client.send_json_rpc[[]string, string]('eth.GetFungibleBalance', [contractAddress, address],
+		eth.default_timeout)!
+}
+
+// OwnerOfFungible (ERC721)
+pub fn owner_of_fungible(mut client RpcWsClient, contractAddress string, tokenId i64) !string {
+	_ := client.send_json_rpc[[]string, string]('eth.OwnerOfFungible', [contractAddress, tokenId],
+		eth.default_timeout)!
+}
+
+// SafeTransferFungible (ERC721)
+pub fn safe_transfer_fungible(mut client RpcWsClient, contractAddress string, from string, to string, tokenId i64) !string {
+	_ := client.send_json_rpc[[]string, string]('eth.SafeTransferFungible', [contractAddress, from, to, tokenId],
+		eth.default_timeout)!
+}
+
+// TransferFungible (ERC721)
+pub fn transfer_fungible(mut client RpcWsClient, contractAddress string, from string, to string, tokenId i64) !string {
+	_ := client.send_json_rpc[[]string, string]('eth.TransferFungible', [contractAddress, from, to, tokenId],
+		eth.default_timeout)!
+}
+
+// SetFungibleApproval (ERC721)
+pub fn set_fungible_approval(mut client RpcWsClient, contractAddress string, from string, to string, approved bool) !string {
+	_ := client.send_json_rpc[[]string, string]('eth.SetFungibleApproval', [contractAddress, from, to, approved],
+		eth.default_timeout)!
+}
+
+// SetFungibleApprovalForAll (ERC721)
+pub fn set_fungible_approval_for_all(mut client RpcWsClient, contractAddress string, from string, to string, approved bool) !string {
+	_ := client.send_json_rpc[[]string, string]('eth.SetFungibleApprovalForAll', [contractAddress, from, to, approved],
+		eth.default_timeout)!
+}
+
+// GetApprovalForFungible (ERC721)
+pub fn get_fungible_approval(mut client RpcWsClient, contractAddress string, from string, to string) !bool {
+	_ := client.send_json_rpc[[]string, string]('eth.GetApprovalForFungible', [contractAddress, from, to],
+		eth.default_timeout)!
+}
+
+// GetApprovalForAllFungible (ERC721)
+pub fn get_fungible_approval_for_all(mut client RpcWsClient, contractAddress string, from string, to string) !bool {
+	_ := client.send_json_rpc[[]string, string]('eth.GetApprovalForAllFungible', [contractAddress, from, to],
 		eth.default_timeout)!
 }
