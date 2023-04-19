86a0e14168cd12c4a17edaeef9bc1acf1c5a5ae7
brandonpille <brandon@threefold.tech>
2023-04-19 10:07:21 +0200

    Formatted tfchain v scripts

--- scripts/tfchain/model_node.v
+++ scripts/tfchain/model_node.v
@@ -2,33 +2,34 @@ module tfchain
 
 pub struct Node {
 pub:
-	version           u32
-	id           	  u32
-	farm_id           u32
-	twin_id           u32
-	resources         Resources
-	location		  Location
-	public_config     OptionPublicConfig
-	created           u64
-	farming_policy    u32
-	interfaces        []Interface
-	certification     struct {
+	version        u32
+	id             u32
+	farm_id        u32
+	twin_id        u32
+	resources      Resources
+	location       Location
+	public_config  OptionPublicConfig
+	created        u64
+	farming_policy u32
+	interfaces     []Interface
+	certification  struct {
 		is_diy       bool
 		is_certified bool
 	}
-	secure_boot       bool
-	vertualized       bool
-	board_serial      OptionBoardSerial
-	connection_price  u32
+
+	secure_boot      bool
+	vertualized      bool
+	board_serial     OptionBoardSerial
+	connection_price u32
 }
 
-pub struct OptionBoardSerial{
+pub struct OptionBoardSerial {
 pub:
-	has_value     bool
-	as_value      string
+	has_value bool
+	as_value  string
 }
 
-pub struct Resources{
+pub struct Resources {
 pub mut:
 	hru u64
 	sru u64
@@ -36,39 +37,40 @@ pub mut:
 	mru u64
 }
 
-pub struct Location{
+pub struct Location {
 pub:
-	city string
-	country string
-	latitude string
+	city      string
+	country   string
+	latitude  string
 	longitude string
 }
 
-pub struct OptionPublicConfig{
-	has_value     bool
-	as_value      PublicConfig
+pub struct OptionPublicConfig {
+	has_value bool
+	as_value  PublicConfig
 }
 
-pub struct PublicConfig{
+pub struct PublicConfig {
 pub:
 	ip4 IP
 	ip6 struct {
 		has_value bool
 		as_value  IP
 	}
+
 	domain struct {
 		has_value bool
 		as_value  string
 	}
 }
 
-pub struct IP{
-	ip string 
+pub struct IP {
+	ip string
 	gw string
 }
 
-pub struct Interface{
+pub struct Interface {
 	name string
-	mac string
-	ips []string
+	mac  string
+	ips  []string
 }
--- scripts/tfchain/tfchain.v
+++ scripts/tfchain/tfchain.v
@@ -7,9 +7,9 @@ const (
 )
 
 [params]
-pub struct Transfer{
+pub struct Transfer {
 pub:
-	amount u64
+	amount      u64
 	destination string
 }
 
@@ -43,7 +43,7 @@ pub struct CreateNodeContract {
 
 [params]
 pub struct CreateRentContract {
-	node_id	u32
+	node_id              u32
 	solution_provider_id ?u64
 }
 
@@ -97,12 +97,12 @@ pub fn new(mut client RpcWsClient) TfChainClient {
 }
 
 pub fn (mut t TfChainClient) load(network string, passphrase string) ! {
-	_ := t.client.send_json_rpc[[]string, string]('tfchain.Load', [network, passphrase], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]string, string]('tfchain.Load', [network, passphrase],
+		tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) transfer(args Transfer) ! {
-	_ := t.client.send_json_rpc[[]Transfer, string]('tfchain.Transfer', [args],
-		tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]Transfer, string]('tfchain.Transfer', [args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) balance(address string) !i64 {
@@ -118,7 +118,8 @@ pub fn (mut t TfChainClient) get_twin(id u32) !Twin {
 }
 
 pub fn (mut t TfChainClient) get_twin_by_pubkey(address string) !u32 {
-	return t.client.send_json_rpc[[]string, u32]('tfchain.GetTwinByPubKey', [address], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]string, u32]('tfchain.GetTwinByPubKey', [address],
+		tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) create_twin(args CreateTwin) !u32 {
@@ -126,7 +127,8 @@ pub fn (mut t TfChainClient) create_twin(args CreateTwin) !u32 {
 }
 
 pub fn (mut t TfChainClient) accept_terms_and_conditions(args AcceptTermsAndConditions) ! {
-	_ := t.client.send_json_rpc[[]AcceptTermsAndConditions, string]('tfchain.AcceptTermsAndConditions', [args], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]AcceptTermsAndConditions, string]('tfchain.AcceptTermsAndConditions',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) get_node(id u32) !Node {
@@ -154,69 +156,91 @@ pub fn (mut t TfChainClient) create_farm(args CreateFarm) ! {
 }
 
 pub fn (mut t TfChainClient) get_contract(contract_id u64) !Contract {
-	return t.client.send_json_rpc[[]u64, Contract]('tfchain.GetContract', [contract_id], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]u64, Contract]('tfchain.GetContract', [
+		contract_id,
+	], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) get_contract_id_by_name_registration(name string) !u64 {
-	return t.client.send_json_rpc[[]string, u64]('tfchain.GetContractIDByNameRegistration', [name], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]string, u64]('tfchain.GetContractIDByNameRegistration',
+		[name], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) get_contract_with_hash(args GetContractWithHash) !u64 {
-	return t.client.send_json_rpc[[]GetContractWithHash, u64]('tfchain.GetContractWithHash', [args], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]GetContractWithHash, u64]('tfchain.GetContractWithHash',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) get_node_contracts(node_id u32) ![]u64 {
-	return t.client.send_json_rpc[[]u32, []u64]('tfchain.GetNodeContracts', [node_id], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]u32, []u64]('tfchain.GetNodeContracts', [node_id],
+		tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) create_name_contract(name string) !u64 {
-	return t.client.send_json_rpc[[]string, u64]('tfchain.CreateNameContract', [name], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]string, u64]('tfchain.CreateNameContract', [name],
+		tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) create_node_contract(args CreateNodeContract) !u64 {
-	return t.client.send_json_rpc[[]CreateNodeContract, u64]('tfchain.CreateNodeContract', [args], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]CreateNodeContract, u64]('tfchain.CreateNodeContract',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) create_rent_contract(args CreateRentContract) !u64 {
-	return t.client.send_json_rpc[[]CreateRentContract, u64]('tfchain.CreateRentContract', [args], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]CreateRentContract, u64]('tfchain.CreateRentContract',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) service_contract_create(args ServiceContractCreate) !u64 {
-	return t.client.send_json_rpc[[]ServiceContractCreate, u64]('tfchain.ServiceContractCreate', [args], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]ServiceContractCreate, u64]('tfchain.ServiceContractCreate',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) service_contract_approve(contract_id u64) !u64 {
-	return t.client.send_json_rpc[[]u64, u64]('tfchain.ServiceContractApprove', [contract_id], tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]u64, u64]('tfchain.ServiceContractApprove', [
+		contract_id,
+	], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) service_contract_bill(args ServiceContractBill) ! {
-	_ := t.client.send_json_rpc[[]ServiceContractBill, string]('tfchain.ServiceContractBill', [args], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]ServiceContractBill, string]('tfchain.ServiceContractBill',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) service_contract_cancel(contract_id u64) ! {
-	_ := t.client.send_json_rpc[[]u64, string]('tfchain.ServiceContractCancel', [contract_id], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]u64, string]('tfchain.ServiceContractCancel', [
+		contract_id,
+	], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) service_contract_reject(contract_id u64) ! {
-	_ := t.client.send_json_rpc[[]u64, string]('tfchain.ServiceContractReject', [contract_id], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]u64, string]('tfchain.ServiceContractReject', [
+		contract_id,
+	], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) service_contract_set_fees(args SetServiceContractFees) ! {
-	_ := t.client.send_json_rpc[[]SetServiceContractFees, string]('tfchain.ServiceContractSetFees', [args], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]SetServiceContractFees, string]('tfchain.ServiceContractSetFees',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) service_contract_set_metadata(args ServiceContractSetMetadata) ! {
-	_ := t.client.send_json_rpc[[]ServiceContractSetMetadata, string]('tfchain.ServiceContractSetMetadata', [args], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]ServiceContractSetMetadata, string]('tfchain.ServiceContractSetMetadata',
+		[args], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) cancel_contract(contract_id u64) ! {
-	_ := t.client.send_json_rpc[[]u64, string]('tfchain.CancelContract', [contract_id], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[]u64, string]('tfchain.CancelContract', [contract_id],
+		tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) batch_cancel_contract(contract_ids []u64) ! {
-	_ := t.client.send_json_rpc[[][]u64, string]('tfchain.BatchCancelContract', [contract_ids], tfchain.default_timeout)!
+	_ := t.client.send_json_rpc[[][]u64, string]('tfchain.BatchCancelContract', [
+		contract_ids,
+	], tfchain.default_timeout)!
 }
 
 pub fn (mut t TfChainClient) get_zos_version() !string {
-	return t.client.send_json_rpc[[]string, string]('tfchain.GetZosVersion', []string{}, tfchain.default_timeout)!
+	return t.client.send_json_rpc[[]string, string]('tfchain.GetZosVersion', []string{},
+		tfchain.default_timeout)!
 }
