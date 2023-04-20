f9d61750b74d5232d9e9432740ba5a22730bfa04
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-04-20 13:22:23 +0200

    v fmt

--- scripts/explorer/grid_proxy.v
+++ scripts/explorer/grid_proxy.v
@@ -1,43 +1,54 @@
 module explorer
 
-import freeflowuniverse.crystallib.rpcwebsocket { RpcWsClient }
+import freeflowuniverse.crystallib.rpcwebsocket
 
 const (
 	default_timeout = 500000
 )
 
 pub fn (mut client Explorer) load(network string) ! {
-	_ := client.send_json_rpc[[]string, string]('explorer.Load', [network], default_timeout)!
+	_ := client.send_json_rpc[[]string, string]('explorer.Load', [network], explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) ping() !string {
-	return client.send_json_rpc[[]string, string]('explorer.Ping', []string{}, default_timeout)!
+	return client.send_json_rpc[[]string, string]('explorer.Ping', []string{}, explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) nodes(params NodesRequestParams) ![]Node {
-	return client.send_json_rpc[[]NodesRequestParams, []Node]('explorer.Nodes', [params], default_timeout)!
+	return client.send_json_rpc[[]NodesRequestParams, []Node]('explorer.Nodes', [
+		params,
+	], explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) farms(params FarmsRequestParams) ![]Farm {
-	return client.send_json_rpc[[]FarmsRequestParams, []Farm]('explorer.Farms', [params], default_timeout)!
+	return client.send_json_rpc[[]FarmsRequestParams, []Farm]('explorer.Farms', [
+		params,
+	], explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) contracts(params ContractsRequestParams) ![]Contract {
-	return client.send_json_rpc[[]ContractsRequestParams, []Contract]('explorer.Contracts', [params], default_timeout)!
+	return client.send_json_rpc[[]ContractsRequestParams, []Contract]('explorer.Contracts',
+		[params], explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) twins(params TwinsRequestParams) ![]Twin {
-	return client.send_json_rpc[[]TwinsRequestParams, []Twin]('explorer.Twins', [params], default_timeout)!
+	return client.send_json_rpc[[]TwinsRequestParams, []Twin]('explorer.Twins', [
+		params,
+	], explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) node(node_id u32) ![]NodeWithNestedCapacity {
-	return client.send_json_rpc[[]u32, []NodeWithNestedCapacity]('explorer.Node', [node_id], default_timeout)!
+	return client.send_json_rpc[[]u32, []NodeWithNestedCapacity]('explorer.Node', [
+		node_id,
+	], explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) node_status(node_id u32) !NodeStatus {
-	return client.send_json_rpc[[]u32, NodeStatus]('explorer.NodeStatus', [node_id], default_timeout)!
+	return client.send_json_rpc[[]u32, NodeStatus]('explorer.NodeStatus', [node_id], explorer.default_timeout)!
 }
 
 pub fn (mut client Explorer) counters(filters StatsFilter) !Counters {
-	return client.send_json_rpc[[]StatsFilter, Counters]('explorer.Counters', [filters], default_timeout)!
+	return client.send_json_rpc[[]StatsFilter, Counters]('explorer.Counters', [
+		filters,
+	], explorer.default_timeout)!
 }
