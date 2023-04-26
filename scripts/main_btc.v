fc0fc3558df0ace96615e0111e3e918fea277c76
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-04-26 16:40:12 +0200

    init btc test Co-authored-by: MarioBassem <mariobassem12@gmail.com>

---
+++ scripts/main_btc.v
@@ -0,0 +1,47 @@
+module main
+
+import btc
+
+fn execute_rpcs(mut client RpcWsClient, mut logger log.Logger, mnemonic string) ! {
+	mut btc_client := btc.new(mut client)
+
+	btc_client.load(btc.Load{
+		host: 'host'
+		user: 'user'
+		pass: 'pass'
+	})!
+
+}
+
+fn main() {
+	mut fp := flag.new_flag_parser(os.args)
+	fp.application('Welcome to the web3_proxy client. The web3_proxy client allows you to execute all remote procedure calls that the web3_proxy server can handle.')
+	fp.limit_free_args(0, 0)!
+	fp.description('')
+	fp.skip_executable()
+	mnemonic := fp.string('mnemonic', `m`, '', 'The mnemonic to be used to call any function')
+	address := fp.string('address', `a`, '${default_server_address}', 'The address of the web3_proxy server to connect to.')
+	debug_log := fp.bool('debug', 0, false, 'By setting this flag the client will print debug logs too.')
+	_ := fp.finalize() or {
+		eprintln(err)
+		println(fp.usage())
+		exit(1)
+	}
+
+	mut logger := log.Logger(&log.Log{
+		level: if debug_log { .debug } else { .info }
+	})
+
+	mut myclient := rpcwebsocket.new_rpcwsclient(address, &logger) or {
+		logger.error('Failed creating rpc websocket client: ${err}')
+		exit(1)
+	}
+
+	_ := spawn myclient.run()
+	
+	
+	execute_rpcs(mut myclient, mut logger, mnemonic) or {
+		logger.error("Failed executing calls: $err")
+		exit(1)
+	}
+}
