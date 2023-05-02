19f6218945f9d1ef450d5fe0652d01611517d21e
brandonpille <brandon@threefold.tech>
2023-05-02 14:23:20 +0200

    Fixed mistake

--- lib/stellar/stellar.v
+++ lib/stellar/stellar.v
@@ -35,15 +35,15 @@ pub fn new(mut client RpcWsClient) StellarClient {
 
 // Load a client, connecting to the rpc endpoint at the given network and loading a keypair from the given secret.
 pub fn (mut s StellarClient) load(args Load) ! {
-	_ := client.send_json_rpc[[]Load, string]('stellar.Load', [args], default_timeout)!
+	_ := s.client.send_json_rpc[[]Load, string]('stellar.Load', [args], default_timeout)!
 }
 
 // Transer an amount of TFT from the loaded account to the destination.
 pub fn (mut s StellarClient) transer(args Transfer) ! {
-	_ := client.send_json_rpc[[]Transfer, string]('stellar.Transfer', [args], default_timeout)!
+	_ := s.client.send_json_rpc[[]Transfer, string]('stellar.Transfer', [args], default_timeout)!
 }
 
 // Balance of an account for TFT on stellar.
 pub fn (mut s StellarClient) balance(address string) ! i64 {
-	return client.send_json_rpc[[]string, i64]('stellar.Balance', [address], default_timeout)!
+	return s.client.send_json_rpc[[]string, i64]('stellar.Balance', [address], default_timeout)!
 }
