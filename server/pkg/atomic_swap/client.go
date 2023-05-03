702d04c9a391dca32baf2c1ab7ac412872613a84
Lee Smet <lee.smet@hotmail.com>
2023-05-03 16:16:39 +0200

    Expose atomic swap buy and sell methods
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- server/pkg/atomic_swap/client.go
+++ server/pkg/atomic_swap/client.go
@@ -18,6 +18,17 @@ type (
 	AtomicSwapState struct {
 		Client *atomicswap.Client
 	}
+
+	// SwapInfo used to start driving an atomic swap
+	SwapInfo struct {
+		// PaymentCurrency string of the currency used to buy TFT
+		PaymentCurrency string `json:"paymentCurrency"`
+		// Amount of TFT to sell (whole units)
+		Amount uint64 `json:"amount"`
+		// Price expressed as the smallest unit of payment currency for 1 TFT. As buyer, this is the
+		// maximum price you are willing to pay for 1 TFT
+		Price uint64 `json:"price"`
+	}
 )
 
 const (
@@ -26,9 +37,10 @@ const (
 )
 
 var (
-	ErrNoEthClient     = errors.New("No ethereum client loaded")
-	ErrNoStellarClient = errors.New("No stellar client loaded")
-	ErrNoNostrClient   = errors.New("No nostr client loaded")
+	ErrAtomicSwapClientNotInitialized = errors.New("Atomic swap client is not initialized")
+	ErrNoEthClient                    = errors.New("No ethereum client loaded")
+	ErrNoStellarClient                = errors.New("No stellar client loaded")
+	ErrNoNostrClient                  = errors.New("No nostr client loaded")
 )
 
 // NewClient creates a new Client ready for use
@@ -75,6 +87,36 @@ func (c *Client) Load(ctx context.Context, conState jsonrpc.State) error {
 	state := State(conState)
 
 	state.Client = cl
+
+	return nil
+}
+
+func (c *Client) Sell(ctx context.Context, conState jsonrpc.State, si SwapInfo) error {
+	state := State(conState)
+	if state.Client == nil {
+		return ErrAtomicSwapClientNotInitialized
+	}
+
+	// TODO: save driver so we can later reference it
+	_, err := state.Client.PlaceSellOrder(ctx, uint(si.Amount), si.PaymentCurrency, uint(si.Price))
+	if err != nil {
+		return errors.Wrap(err, "could not place sell order")
+	}
+
+	return nil
+}
+
+func (c *Client) Buy(ctx context.Context, conState jsonrpc.State, si SwapInfo) error {
+	state := State(conState)
+	if state.Client == nil {
+		return ErrAtomicSwapClientNotInitialized
+	}
+
+	// TODO: save driver so we can later reference it
+	_, err := state.Client.AttemptBuy(ctx, uint(si.Amount), si.PaymentCurrency, uint(si.Price))
+	if err != nil {
+		return errors.Wrap(err, "could not start buying")
+	}
 
 	return nil
 }
