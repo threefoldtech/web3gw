bae51efa9483a4d2e405c20310b2be0604f12a84
Ali Chaddad <chaddada@incubaid.com>
2023-05-03 14:13:16 +0300

    init sftpgo

--- server/main.go
+++ server/main.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 
 	"github.com/LeeSmet/go-jsonrpc"
+	"github.com/drakkan/sftpgo/v2/pkg/service"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/threefoldtech/web3_proxy/server/pkg"
@@ -24,15 +25,32 @@ import (
 	"github.com/threefoldtech/web3_proxy/server/pkg/tfgrid"
 )
 
+const (
+	defaultSFTPConfigFile        = "sftpgo.json"
+	defaultSFTPLogFile           = "sftpgo.log"
+	defaultSFTPLogMaxSize        = 10
+	defaultSFTPLogMaxBackup      = 5
+	defaultSFTPLogMaxAge         = 28
+	defaultSFTPLogCompress       = false
+	defaultSFTPLogLevel          = "info"
+	defaultSFTPLogUTCTime        = false
+	defaultSFTPLoadDataFrom      = ""
+	defaultSFTPLoadDataMode      = 1
+	defaultSFTPLoadDataQuotaScan = 0
+	defaultSFTPLoadDataClean     = false
+)
+
 func main() {
 	var enableIpfs, debug bool
 	var port, ipfsPort uint64
+	var sftpConfigDir string
 
 	flag.Uint64Var(&port, "port", 8080, "RPC Port to listen on")
 	flag.Uint64Var(&ipfsPort, "ipfs-port", 4001, "IPFS Port to listen on")
 
 	flag.BoolVar(&enableIpfs, "ipfs", false, "Enable IPFS")
 	flag.BoolVar(&debug, "debug", false, "sets debug level log output")
+	flag.StringVar(&sftpConfigDir, "sftp-config-dir", "", "enable SFTP server")
 
 	flag.Parse()
 
@@ -73,6 +91,39 @@ func main() {
 				panic(err)
 			}
 			rpcServer.Register("ipfs", ipfs.NewClient(lite))
+		}()
+	}
+
+	if sftpConfigDir != "" {
+		sftpLogLevel := defaultSFTPLogLevel
+		if debug {
+			sftpLogLevel = "debug"
+		}
+		log.Info().Msg("Starting SFTP server")
+		go func() {
+			service := service.Service{
+				ConfigDir:         sftpConfigDir,
+				ConfigFile:        defaultSFTPConfigFile,
+				LogFilePath:       defaultSFTPLogFile,
+				LogMaxSize:        defaultSFTPLogMaxSize,
+				LogMaxBackups:     defaultSFTPLogMaxBackup,
+				LogMaxAge:         defaultSFTPLogMaxAge,
+				LogCompress:       defaultSFTPLogCompress,
+				LogLevel:          sftpLogLevel,
+				LogUTCTime:        defaultSFTPLogUTCTime,
+				LoadDataFrom:      defaultSFTPLoadDataFrom,
+				LoadDataMode:      defaultSFTPLoadDataMode,
+				LoadDataQuotaScan: defaultSFTPLoadDataQuotaScan,
+				LoadDataClean:     defaultSFTPLoadDataClean,
+				Shutdown:          make(chan bool),
+			}
+			if err := service.Start(false); err == nil {
+				service.Wait()
+				if service.Error == nil {
+					os.Exit(0)
+				}
+			}
+			os.Exit(1)
 		}()
 	}
 
