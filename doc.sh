d9197e6d5b326eef19990b0b6972b8f247da5098
timurgordon <timurgordon@gmail.com>
2023-05-05 14:10:15 +0300

    create document per domain

--- doc.sh
+++ doc.sh
@@ -7,6 +7,22 @@ pushd manual
 bash run.sh
 popd
 
+# compile openrpc cli binary
+v ~/.vmodules/freeflowuniverse/crystallib/openrpc/cli
+OPENRPC_CLI=~/.vmodules/freeflowuniverse/crystallib/openrpc/cli/cli
+
+# generate doc for entire api
+$OPENRPC_CLI docgen -t "Web3Proxy JSON-RPC API" -p -o server lib
+
+for file in lib/*
+do
+    # generate individual docs per domain
+    name=${file##*/}
+    $OPENRPC_CLI docgen -t "$name JSON-RPC API" -p -o server/pkg/$name $file
+done
+
+# v run ~/.vmodules/freeflowuniverse/crystallib/openrpc/cli/cli.v docgen -t "Web3Proxy JSON-RPC API" -p -o server lib
+
 # v fmt -w .
 # v doc -m -f html . -readme -comments -no-timestamp
 
--- install.sh
+++ install.sh
@@ -5,3 +5,9 @@ DIR_OF_THIS_SCRIPT="$( dirname "$SOURCE" )"
 ABS_DIR_OF_SCRIPT="$( realpath $DIR_OF_THIS_SCRIPT )"
 mkdir -p ~/.vmodules/threefoldtech
 ln -s $ABS_DIR_OF_SCRIPT/lib ~/.vmodules/threefoldtech/threebot
+
+# install crystallib
+if !(v list | grep -q 'freeflowuniverse.crystallib'); then
+    git clone https://github.com/freeflowuniverse/crystallib.git ~/.vmodules/freeflowuniverse/crystallib
+fi
+
