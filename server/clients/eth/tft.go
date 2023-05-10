16aa4465eaa7436861a5adadbab088049008b467
dylanverstraete <dylan_verstraete1@hotmail.com>
2023-05-10 09:36:35 +0200

    chore: import token contract from threefoldfoundation

--- server/clients/eth/tft.go
+++ server/clients/eth/tft.go
@@ -8,7 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/pkg/errors"
-	"github.com/threefoldtech/web3_proxy/server/clients/eth/tft"
+	tft "github.com/threefoldfoundation/tft/bridge/stellar/contracts/tokenv1"
 )
 
 const (
@@ -20,7 +20,7 @@ func (c *Client) TransferTftEth(ctx context.Context, destination string, amount 
 }
 
 func (c *Client) WithdrawEthTftToStellar(ctx context.Context, destination string, amount int64) (string, error) {
-	tft, err := tft.NewTft(common.HexToAddress(PublicEthTftContractAddress), c.Eth)
+	tft, err := tft.NewToken(common.HexToAddress(PublicEthTftContractAddress), c.Eth)
 	if err != nil {
 		return "", err
 	}
@@ -55,7 +55,7 @@ func (c *Client) WithdrawEthTftToStellar(ctx context.Context, destination string
 }
 
 func (c *Client) GetTftBalance(ctx context.Context) (*big.Int, error) {
-	tft, err := tft.NewTft(common.HexToAddress(PublicEthTftContractAddress), c.Eth)
+	tft, err := tft.NewToken(common.HexToAddress(PublicEthTftContractAddress), c.Eth)
 	if err != nil {
 		return nil, err
 	}
--- server/clients/eth/tft/generate.go
+++
@@ -1,3 +0,0 @@
-package tft
-
-//go:generate abigen --abi ../contracts/TFT/tft.abi --pkg tft --out tft.go
