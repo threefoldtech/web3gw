ed1b18bd3b16e935a571dbe05dd1f5f3dbcbf9ef
brandonpille <brandon@threefold.tech>
2023-05-12 08:58:27 +0200

    Set default server address to websocket

--- examples/nostr/main_nostr_chat_consumer.v
+++ examples/nostr/main_nostr_chat_consumer.v
@@ -10,7 +10,7 @@ import os
 import time
 
 const (
-	default_server_address = 'http://127.0.0.1:8080'
+	default_server_address = 'ws://127.0.0.1:8080'
 )
 
 fn subscribe_messages(mut client RpcWsClient, mut logger log.Logger, secret string) ! {
