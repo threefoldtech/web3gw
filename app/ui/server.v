87ddc0713cae8a03435f0e2461513ea953eadc0e
timurgordon <timurgordon@gmail.com>
2023-05-18 16:48:03 +0300

    update sidebar nav

--- app/ui/server.v
+++ app/ui/server.v
@@ -3,8 +3,6 @@ module main
 import os
 import vweb
 import time
-import chat
-// import threefoldtech.chat
 import freeflowuniverse.crystallib.pathlib
 
 struct App {
@@ -15,18 +13,17 @@ struct App {
 pub fn (mut app App) before_request() {
 	referer := app.get_header('Referer')
 	hx_request := app.get_header('Hx-Request') == 'true'
-	if !(hx_request) && !app.req.url.contains('static') && !app.req.url.contains('/playground'){
+	if !hx_request && !app.req.url.contains('static') {
 		app.index()
 	}
 }
 
 struct NavItem {
 	label string
-	url string
+	url   string
 }
 
 pub fn (mut app App) index() vweb.Result {
-
 	route := if app.req.url == '/' {
 		'/dashboard'
 	} else {
@@ -43,16 +40,16 @@ pub fn (mut app App) index() vweb.Result {
 		},
 		NavItem{
 			label: 'Manual'
-			url: 'https://threefoldtech.github.io/web3_proxy/index.html'
-		},
-		NavItem{
-			label: 'Manual2'
-			url: '/manual2'
+			url: '/manual'
 		},
 		NavItem{
 			label: 'Playground'
-			url: '/playground2'
-		}
+			url: '/playground'
+		},
+		NavItem{
+			label: 'Console'
+			url: '/chat'
+		},
 	]
 
 	server_ip := '127.0.0.1:8000'
@@ -70,11 +67,10 @@ fn (mut app App) server_is_running() bool {
 }
 
 pub fn main() {
-
 	mut app := &App{
-        controllers: [
-			vweb.controller('/chat', &chat.Chat{}),
-        ]
+		controllers: [
+			vweb.controller('/manual', &Manual{}),
+		]
 	}
 	// app.mount_static_folder_at('../build/static', '/static')
 	// playground_
@@ -88,7 +84,7 @@ pub fn mount_playground() ! {
 	os.chdir(os.dir(@FILE))!
 	playground_path := pathlib.get('../playground').path
 	static_path := pathlib.get('static').path
-	os.execute('ln -s $playground_path/css/* $static_path/css/')
-	os.execute('ln -s $static_path/js/* static/js/')
+	os.execute('ln -s ${playground_path}/css/* ${static_path}/css/')
+	os.execute('ln -s ${static_path}/js/* static/js/')
 	os.execute('cp ../playground/index.html templates')
 }
