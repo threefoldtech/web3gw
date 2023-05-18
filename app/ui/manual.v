a78169821f4efe0fbe2fad8abc653c9b9c79d0fb
timurgordon <timurgordon@gmail.com>
2023-05-18 16:47:15 +0300

    fix manual and use iframe

--- app/ui/manual.v
+++ app/ui/manual.v
@@ -3,22 +3,27 @@ module main
 import os
 import vweb
 import time
-// import threefoldtech.chat
-import freeflowuniverse.crystallib.pathlib
 
-pub fn (mut app App) manual() vweb.Result {
-	iframe_source := "https://threefoldtech.github.io/web3_proxy/index.html"
-	return $vweb.html()
+struct Manual {
+	vweb.Context
+mut:
+	path string
 }
 
-[GET; '/manual/:path']
-pub fn (mut app App) manual_path(path string) vweb.Result {
+pub fn (mut manual Manual) before_request() {
+	url := manual.req.url.all_after_first('/')
+	path := url.trim_string_left('manual')
 	println(path)
-	manual_url := "https://threefoldtech.github.io/web3_proxy"
-	iframe_source := if path == '' {
-		'$manual_url/index.html' 
+	manual.path = path
+	manual.index()
+}
+
+pub fn (mut manual Manual) index() vweb.Result {
+	manual_url := 'https://threefoldtech.github.io/web3_proxy'
+	iframe_source := if manual.path == '' {
+		'${manual_url}/index.html'
 	} else {
-		'$manual_url/$path'
+		'${manual_url}/${manual.path}'
 	}
-	return app.html($tmpl('templates/manual2.html'))
+	return manual.html($tmpl('templates/manual.html'))
 }
