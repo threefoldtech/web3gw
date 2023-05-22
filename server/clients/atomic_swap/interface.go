5ebd74c2cca90ad304b2f6cd59228708e93200f7
Lee Smet <lee.smet@hotmail.com>
2023-05-22 17:24:54 +0200

    Start generalizing data
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ server/clients/atomic_swap/interface.go
@@ -0,0 +1,41 @@
+package atomicswap
+
+import (
+	"context"
+	"crypto/sha256"
+)
+
+type (
+	// BuyChain holds all logic regarding the chain used to pay for TFT
+	BuyChain interface {
+		// InitPayment initializes a payment transaction on the chain
+		InitPayment(ctx context.Context, tftAmount uint, tftPrice float64) (any, error)
+		// ValidateInitPaymentResult validates the result of an init payment call
+		ValidateInitPaymentResult(ctx context.Context, initResult any, details NegotiatedTrade) error
+		// Claim payment on chain
+		Claim(ctx context.Context, initResult any, secret SwapSecret) error
+	}
+
+	// SellChain holds all logic regarding the chain on which TFT are sold
+	SellChain interface {
+		// InitTFTTransfer locks TFT's in the contract
+		InitTFTTransfer(ctx context.Context, details NegotiatedTrade, sharedSecret SwapSecretHash) (any, error)
+		// ValidateTFTTranser validates the locked TFT's
+		ValidateTFTTranser(ctx context.Context, initTransferResult any, sharedSecret SwapSecretHash) error
+		// ClaimTFT claims the locked TFT's
+		ClaimTFT(ctx context.Context, initTransferResult any, secret SwapSecret) error
+	}
+
+	// NegotiatedTrade holds data both parties agreed on in a trade
+	NegotiatedTrade struct {
+		// Amount of TFT
+		Amount uint
+		// Price of 1 TFT
+		Price uint
+	}
+
+	// SwapSecret generated for an atomic swap
+	SwapSecret [32]byte
+	// SwapSecretHash is the sha256 hash of the swap secret used in a trade
+	SwapSecretHash [sha256.Size]byte
+)
