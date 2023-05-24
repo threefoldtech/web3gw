ba50336cb7ac058e7d06d1018c304be208adc922
Lee Smet <lee.smet@hotmail.com>
2023-05-24 18:52:36 +0200

    Allow initialization of stellar driver
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

--- server/clients/atomic_swap/stellar.go
+++ server/clients/atomic_swap/stellar.go
@@ -31,6 +31,15 @@ type (
 	}
 )
 
+func initTFTDriver(stellar *stellargoclient.Client) *StellarDriver {
+	return &StellarDriver{
+		stellar:           stellar,
+		horizonClient:     stellar.GetHorizonClient(),
+		networkPassphrase: stellar.GetStellarNetworkPassphrase(),
+		asset:             stellar.GetTftAsset(),
+	}
+}
+
 // InitTFTTransfer implements SellChain
 func (s *StellarDriver) InitTFTTransfer(ctx context.Context, details NegotiatedTrade, sharedSecret SwapSecretHash, destination string) (any, error) {
 	kp := s.stellar.KeyPair()
@@ -101,22 +110,4 @@ func (s *StellarDriver) ClaimTFT(ctx context.Context, initTransferResult any, se
 	}
 
 	return nil
-}
-
-// Parse the stellar testnet TFT asset
-func mustStellarTestnetTftAsset() txnbuild.Asset {
-	a, err := txnbuild.ParseAssetString("TFT:GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3")
-	if err != nil {
-		panic(err)
-	}
-	return a
-}
-
-// Parse the stellar mainnet TFT asset
-func mustStellarTftAsset() txnbuild.Asset {
-	a, err := txnbuild.ParseAssetString("TFT:GBOVQKJYHXRR3DX6NOX2RRYFRCUMSADGDESTDNBDS6CDVLGVESRTAC47")
-	if err != nil {
-		panic(err)
-	}
-	return a
 }
