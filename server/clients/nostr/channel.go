b5a6cbf48b6adbacdb14bd296580010370f6beb6
Lee Smet <lee.smet@hotmail.com>
2023-05-30 16:50:54 +0200

    Implement nostr groups support in nostr pkg
    Signed-off-by: Lee Smet <lee.smet@hotmail.com>

---
+++ server/clients/nostr/channel.go
@@ -0,0 +1,80 @@
+package nostr
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/pkg/errors"
+)
+
+type (
+	Channel struct {
+		Name    string `json:"name"`
+		About   string `json:"about"`
+		Picture string `json:"picture"`
+	}
+
+	ChannelMessage struct {
+		// Content of the message
+		Content string
+		// ReplyTo is either the ID of a message to reply to, or the ID of the channel create message of the channel to post in
+		// if this is a root message in the channel
+		ReplyTo string
+	}
+)
+
+const (
+	// kindCreateChannel creates a channel
+	kindCreateChannel = 40
+	// kindSetChannelMetadata updates channel metadata
+	kindSetChannelMetadata = 41
+	// kindCreateChannelMessage creates a message in a channel
+	kindCreateChannelMessage = 42
+	// kindHideChannelMessage hides a message in the channel
+	kindHideChannelMessage = 43
+	// kindMuteChannelUser mutes a channel user for the sending user
+	kindMuteChanneluser = 44
+)
+
+// CreateChannel creates a new channel
+func (c *Client) CreateChannel(ctx context.Context, tags []string, content Channel) error {
+	if content.Name == "" {
+		return errors.New("Channel must have a name")
+	}
+	marshalledContent, err := json.Marshal(content)
+	if err != nil {
+		return errors.Wrap(err, "could not encode metadata")
+	}
+	return c.publishEventToRelays(ctx, kindCreateChannel, [][]string{tags}, string(marshalledContent))
+}
+
+// UpdateChannelMetadata updates the channel metdata. ChannelID is the event ID of the create channel event used to create the channel to update
+func (c *Client) UpdateChannelMetadata(ctx context.Context, tags []string, channelID string, content Channel) error {
+	if content.Name == "" {
+		return errors.New("Channel must have a name")
+	}
+	marshalledContent, err := json.Marshal(content)
+	if err != nil {
+		return errors.Wrap(err, "could not encode metadata")
+	}
+	return c.publishEventToRelays(ctx, kindSetChannelMetadata, [][]string{tags, {"e", channelID}}, string(marshalledContent))
+}
+
+// CreateChannelMessage creates a message in channel. If replyTo is the empty string, it is marked as a root
+func (c *Client) CreateChannelMessage(ctx context.Context, tags []string, message ChannelMessage) error {
+	if message.Content == "" {
+		return errors.New("Refusing to submit empty message")
+	}
+	return c.publishEventToRelays(ctx, kindSetChannelMetadata, [][]string{tags, {"e", message.ReplyTo}}, message.Content)
+}
+
+// HideMessage marks a message as hidden for the user. It should be noted that properly handling this is mostly up to the clients
+func (c *Client) HideMessage(ctx context.Context, tags []string, messageID string, content string) error {
+	return c.publishEventToRelays(ctx, kindSetChannelMetadata, [][]string{tags, {"e", messageID}}, content)
+}
+
+// MuteUser marks a user as muted for the current user. It should be noted that properly handling this is mostly up to the clients.
+// The user to mute is identified by it's pubkey
+func (c *Client) MuteUser(ctx context.Context, tags []string, user string, content string) error {
+	return c.publishEventToRelays(ctx, kindSetChannelMetadata, [][]string{tags, {"p", user}}, content)
+}
