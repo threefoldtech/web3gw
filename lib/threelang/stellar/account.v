c7092f5f86724756269ce252791a6edd55673913
mario <mariobassem12@gmail.com>
2023-07-10 16:28:49 +0300

    add remaining stellar actions

--- lib/threelang/stellar/account.v
+++ lib/threelang/stellar/account.v
@@ -7,6 +7,37 @@ fn (mut h StellarHandler) account(action Action) ! {
 		'address' {
 			res := h.client.address()!
 
+			h.logger.info(res)
+		}
+		'create' {
+			network := action.params.get_default('network', 'testnet')!
+
+			res := h.client.create_account(network)!
+
+			h.logger.info(res)
+		}
+		'transactions' {
+			account := action.params.get_default('account', '')!
+			limit := action.params.get_u32_default('limit', 10)!
+			include_failed := action.params.get_default_false('include_failed')
+			cursor := action.params.get_default('cursor', '')!
+			ascending := action.params.get_default_false('ascending')
+
+			res := h.client.transactions(
+				account: account
+				limit: limit
+				include_failed: include_failed
+				cursor: cursor
+				ascending: ascending
+			)!
+
+			h.logger.info('Transactions: ${res}')
+		}
+		'data' {
+			account := action.params.get('account')!
+
+			res := h.client.account_data(account)!
+
 			h.logger.info('${res}')
 		}
 		else {
