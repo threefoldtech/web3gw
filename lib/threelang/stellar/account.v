eeab14d98d4d9775d7a3721df503bf5f3fec47d8
mario <mariobassem12@gmail.com>
2023-07-10 15:57:03 +0300

    remove money related actions
    Signed-off-by: mario <mariobassem12@gmail.com>

--- lib/threelang/stellar/account.v
+++ lib/threelang/stellar/account.v
@@ -9,15 +9,6 @@ fn (mut h StellarHandler) account(action Action) ! {
 
 			h.logger.info('${res}')
 		}
-		'transfer' {
-			amount := action.params.get('amount')!
-			destination := action.params.get('destination')!
-			memo := action.params.get_default('memo', '')!
-
-			res := h.client.transfer(amount: amount, destination: destination, memo: memo)!
-
-			h.logger.info('${res}')
-		}
 		else {
 			return error('account action ${action.name} is invalid')
 		}
--- manual/src/threelang/stellar/stellar_actions.md
+++ manual/src/threelang/stellar/stellar_actions.md
@@ -1,9 +1,6 @@
 # Stellar Actions
 
-- [Load](./load.md)
 - [Address](./address.md)
-- [Transfer](./transfer.md)
-- [Balance](./balance.md)
 - [Bridge to Eth](./bridge_to_eth.md)
 - [Bridge to TFChain](./bridge_to_tfchain.md)
 - [Await transaction on Eth bridge](./await_transaction_on_eth_bridge.md)
