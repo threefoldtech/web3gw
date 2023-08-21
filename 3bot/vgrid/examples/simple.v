b31b9c6aaa8682e1908d8bca04362d596209b2b6
sameh-farouk <samehabouelsaad@gmail.com>
2023-08-21 13:50:32 +0300

    fix simple example

--- 3bot/vgrid/examples/simple.v
+++ 3bot/vgrid/examples/simple.v
@@ -1,22 +1,22 @@
 import threefoldtech.vgrid.gridproxy
 
-fn do()?{
-	mut gp_client := gridproxy.get(.test, true)
-	farms := gp_client.get_farms()? 
+fn do()!{
+	mut gp_client := gridproxy.get(.test, true)!
+	farms := gp_client.get_farms()!
 	// println(farms)
 
 	// get gateway list
-	gateways := gp_client.get_gateways()?
+	gateways := gp_client.get_gateways()!
 	// get contract list
-	contracts := gp_client.get_contracts()?
+	contracts := gp_client.get_contracts()!
 	// get grid stats
-	stats := gp_client.get_stats()?
+	stats := gp_client.get_stats()!
 	// println(stats)
 	// get twins
-	twins := gp_client.get_twins()?
+	twins := gp_client.get_twins()!
 
 	// get node list
-	nodes := gp_client.get_nodes(dedicated: true,free_ips: u64(1))?
+	nodes := gp_client.get_nodes(dedicated: true,free_ips: u64(1))!
 	println(nodes)
 
 
