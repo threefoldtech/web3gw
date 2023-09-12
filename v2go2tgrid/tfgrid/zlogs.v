a2a780c2b838fdddbb7c6a563727586987c9764b
kassem <omarksm09@gmail.com>
2023-09-12 15:49:58 +0300

    format and explain type

--- v2go2tgrid/tfgrid/zlogs.v
+++ v2go2tgrid/tfgrid/zlogs.v
@@ -3,16 +3,14 @@ module tfgrid
 import json
 
 pub struct ZLogs {
-	// zmachine to stream logs of
-	zmachine string
-	// the `target` location to stream the logs to, it must be a redis or web-socket url
-	output   string 
+	zmachine string // zmachine name to stream logs of
+	output   string // the `target` location to stream the logs to, it must be a redis or web-socket url
 }
 
 pub fn (z ZLogs) challenge() string {
 	mut output := ''
-	output += z.zmachine 
-	output += z.output  
+	output += z.zmachine
+	output += z.output
 
 	return output
 }
