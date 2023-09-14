586f268cc288d3f7904bd40ce496aa054c291d88
kassem <omarksm09@gmail.com>
2023-09-14 14:32:18 +0300

    WIP: update example

--- v2go2tgrid/tfgrid/examples/deploy_gw_fqdn.v
+++ v2go2tgrid/tfgrid/examples/deploy_gw_fqdn.v
@@ -8,17 +8,21 @@ fn main() {
 		level: .debug
 	})
 
-	mnemonics := '<YOUR MNEMONICS>'
+	mnemonics := tfgrid.get_mnemonics() or {
+		logger.error(err.str())
+		exit(1)
+	}
 	chain_network := tfgrid.ChainNetwork.dev // User your desired network
 	mut deployer := tfgrid.new_deployer(mnemonics, chain_network)!
 
 	gw := tfgrid.GatewayFQDNProxy{
 		tls_passthrough: false
 		backends: ['http://1.1.1.1:9000']
-		fqdn: 'hamada1.3x0.me'
+		fqdn: 'hamada3.3x0.me'
 	}
 	wl := gw.to_workload(name: 'mywlname')
-
+	node_id := u32(11)
+	
 	signature_requirement := tfgrid.SignatureRequirement{
 		weight_required: 1
 		requests: [
@@ -34,6 +38,8 @@ fn main() {
 		signature_requirement: signature_requirement
 	)
 
-	node_contract_id := deployer.deploy(15, mut deployment, '', 0)!
+
+
+	node_contract_id := deployer.deploy(11, mut deployment, '', 0)!
 	logger.info('node contract created with id ${node_contract_id}')
 }
